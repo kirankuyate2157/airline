0eee28d2cf9012362cd898883ae7bd9cf9f861c5
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2024-06-29 15:25:49 +0530

    server setup

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" })); //json upload limit to save server from crash..
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //url data understanding
+app.use(express.static("public")); //store file direct on server for public access
+app.use(cookieParser()); //cookie  set get operations
+
+pp.get("/", (req, res) => {
+    res.send("hey i back end api started now testing .... 🍻⏳");
+  });
+  
+  export { app };
+
+
---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env",
+});
+import { app } from "./app.js";
+import connectDB from "./db/mongoDB.js";
+
+app.listen(process.env.PORT || 8080, () => {
+  connectDB();
+  console.log(
+    "server listening on port http://localhost:" + process.env.PORT + " ✅"
+  );
+});
+
+
